0818619a05a968b116742f6b959bb59c90fad8fb
Justin Ethier <justin.ethier@gmail.com>
2020-09-08 18:44:17 -0400

    Initial file

---
+++ package.scm
@@ -0,0 +1,18 @@
+(package
+ (name           srfi-173)
+ (version        0.3)
+ (license        "MIT")
+ (authors        "Amirouche Boubekki")
+ (maintainers    "Justin Ethier <justin.ethier at gmail dot com>")
+ (description    "Hooks")
+ (tags           "srfi" "language extensions")
+ (docs           "https://github.com/cyclone-scheme/cyclone-winds/wiki/srfi-173")
+ (test           "tests.scm")
+ (dependencies (srfi-145))
+ (test-dependencies ())
+ (foreign-dependencies ())
+
+ (library
+     (name (srfi 173))
+   (description "Hooks")))
+
